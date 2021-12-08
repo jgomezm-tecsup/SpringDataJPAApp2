bbdd3771821920d5226ea1477832c54e453fa794
Jaime <you@example.com>
2021-12-07 20:06:00 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
+++ src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
@@ -1,12 +1,17 @@
 package pe.edu.tecsup.springbootapp.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nombre;
