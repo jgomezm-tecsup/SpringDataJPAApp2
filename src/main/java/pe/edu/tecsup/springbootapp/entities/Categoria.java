cec26503bdeb2f0cef96a39fad0d3e64735de154
Jaime <you@example.com>
2021-12-07 20:04:22 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
+++ src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
@@ -1,5 +1,10 @@
 package pe.edu.tecsup.springbootapp.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
 public class Categoria {
 	
 	private Long id;
