ddfe174065b1f61632c519d6fd3f7129e8000e3b
Jaime <you@example.com>
2021-12-07 20:25:04 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepository.java
@@ -1,13 +1,21 @@
 package pe.edu.tecsup.springbootapp.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import pe.edu.tecsup.springbootapp.entities.Categoria;
 
 public interface CategoriaRepository 
 		extends CrudRepository<Categoria, Long> {
-	
+
 	// CRUD : Create, Read , Update and Delete 
 	
+	@Override
+	List<Categoria> findAll();
 
+	@Override
+	Optional<Categoria> findById(Long id);
+	
 }
