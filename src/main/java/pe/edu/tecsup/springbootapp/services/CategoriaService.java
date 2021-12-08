cdf66eb0c9efcacb46d61ec419d91a8b815a440e
Jaime <you@example.com>
2021-12-07 20:27:32 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/springbootapp/services/CategoriaService.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.springbootapp.services;
+
+import java.util.List;
+
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+
+public interface CategoriaService {
+
+	List<Categoria> findAll();
+}
