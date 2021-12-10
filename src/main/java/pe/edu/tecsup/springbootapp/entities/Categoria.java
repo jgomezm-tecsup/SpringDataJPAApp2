4dd1d3559ab97acb464392bfab13ba1883ec5ef6
Jaime <you@example.com>
2021-12-09 19:12:20 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
+++ src/main/java/pe/edu/tecsup/springbootapp/entities/Categoria.java
@@ -1,9 +1,13 @@
 package pe.edu.tecsup.springbootapp.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +21,18 @@ public class Categoria {
 	private String nombre;
 	
 	private Integer orden;
+
+	@OneToMany(mappedBy="categoria")
+	private List<Producto> productos = new ArrayList<>();
+	
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
 
 	public Long getId() {
 		return id;
